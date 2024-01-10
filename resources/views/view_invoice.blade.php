eccca6028f6b80733b70fdf6abd804daa13334a3
Ramzan8898 <muhammadramzan3043@gmail.com>
2024-01-10 16:47:04 +0500

    working on print

--- resources/views/view_invoice.blade.php
+++ resources/views/view_invoice.blade.php
@@ -1,40 +1,58 @@
 @extends('index')
 @section('content')
 
-<div class="modal-header">
+<style type="text/css">
+	@media print {
+		nav , .print_btn , .back {
+			display: none;
+		}
+		.shop {
+			display: block !important;
+		}
+	}
+/*	.shop{
+		display: none;
+	}*/
+</style>
+<!-- <div class="modal-header">
 	<h1 class="modal-title fs-5" id="staticBackdropLabel">Invoice</h1>
 	<button type="button" class="btn-close" aria-label="Close"></button>
-</div>
+</div> -->
 <div class="modal-body ">
 	<div class="container">
-		<div class="row1" style="display:flex;">
+		<div class="row1" style="display:flex;flex-direction: column;">
 			<div class="row">
-			<!--<div class="span4 d-flex " style="gap: 8px; margin-right: -100px;">
-							
-					<address style="text-align:right;margin-top: 0px ;">
-						<strong style="font-size: 32px;">جیو برتن سٹور
+				<div class="shop">
+					<div class="span4 d-flex" style="gap: 8px;">
+
+						<address style="text-align:right;margin-top: 0px ;">
+							<strong style="font-size: 32px;">جیو برتن سٹور
 							</strong><br>
 
-						<p style="font-size:24px;">اندرون گلی  ،ظہور پلازہ <br>
-								نوری گیٹ سرگودھا </p>
-						<p>فون نمبر: 6051935-0300</p>
-					</address>
-					<img src="{{asset('/geo-news-logo.png')}}" class="img-rounded logo" width="80" height="90">
-				</div> -->
-				<div class="span4 well">
+							<p style="font-size:24px;">اندرون گلی  ،ظہور پلازہ <br>
+							نوری گیٹ سرگودھا </p>
+							<p>فون نمبر: 6051935-0300</p>
+						</address>
+						<img src="{{asset('/geo-news-logo.png')}}" class="img-rounded logo" width="80" height="90">
+					</div> 
+				</div>
+				<a href="{{url('/invoices')}}" class="btn btn-success back" style="width:fit-content;margin-left: 20px;">Back</a>
+				<button class="btn btn-success print_btn" style="width:fit-content;" onclick="window.print('.print')">Print</button>
+
+				<div class="span4 well" style="display:flex; justify-content:flex-end;">
 					<table class="invoice-head">
 						<tbody>
 							<tr>
-								<td>{{$invoice->customer_name}}</td>
-								<td class="pull-right"><strong>کسٹمر </strong></td>
+								<td style="font-size: 24px;display:flex;justify-content: end;">{{$invoice->customer_name}}</td>
+								<td style="font-size: 24px"><strong>:کسٹمر </strong></td>
 							</tr>
 							<tr>
-								<td>{{$invoice->invoice_number}}</td>
-								<td class="pull-right"><strong>انوايس #</strong></td>
+								<td  style="font-size: 24px;display:flex;justify-content: end;">{{$invoice->invoice_number}}</td>
+								<td class="pull-right" style="font-size: 24px"><strong>:#انوايس </strong></td>
 							</tr>
 							<tr>
-								<td>{{$invoice->created_at}}</td>
-								<td class="pull-right"><strong>تاریخ</strong></td>
+								<td style="font-size: 20px;display:flex;justify-content: end;">{{$invoice->created_at}}</td>
+								<td class="pull-right" style="font-size: 24px"><strong>:تاریخ</strong></td>
 							</tr>
 
 						</tbody>
@@ -46,31 +64,29 @@
 					<table class="table table-bordered">
 						<thead>
 							<tr>
-								<th>مصنوعات</th>
-								<th>قیمت</th>
-								<th>مقدار</th>
-								<th>کل</th>
+								<th style="font-size:24px;">کل</th>
+								<th style="font-size:24px;">مقدار</th>
+								<th style="font-size:24px;">قیمت</th>
+								<th style="font-size:24px;">مصنوعات</th>
 							</tr>
 						</thead>
 						<tbody>
 							@foreach($inv_prod as $pro)
 							<tr>
-								<td>{{$pro->product_name}}</td>
-								<td>{{$pro->product_price}}</td>
-								<td>{{$pro->product_qty}}</td>
 								<td>{{$pro->product_total}}</td>
+								<td>{{$pro->product_qty}}</td>
+								<td>{{$pro->product_price}}</td>
+								<td>{{$pro->product_name}}</td>
 							</tr>
 							@endforeach
 							<tr><td colspan="4"></td></tr>
 							<tr>
-								<td colspan="2">&nbsp;</td>
-								<td><strong>Sub Total</strong></td>
 								<td><strong>{{$invoice->sub_total}}</strong></td>
+								<td><strong>ذیلی کل</strong></td>
 							</tr>
 							<tr>
-								<td colspan="2">&nbsp;</td>
-								<td><strong>Total</strong></td>
 								<td><strong>{{$invoice->total}}</strong></td>
+								<td><strong>ٹوٹل</strong></td>
 							</tr>
 						</tbody>
 					</table>
@@ -81,10 +97,8 @@
 					<h5 style="text-align:center;">Thank You!</h5>
 				</div>
 			</div>
+
 		</div>
 	</div>
 </div>
-
-<!-- Invoice View Model End  -->
-
 @endsection
