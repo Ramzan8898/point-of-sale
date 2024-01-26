56a855fbb63d7b96e08345f2cbdfbd8d8664df51
Ramzan8898 <muhammadramzan3043@gmail.com>
2024-01-26 12:12:58 +0500

    sidebar move to right completely

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,14 +3,12 @@
     <a class="navbar-brand mb-5" href="{{url('/')}}">
       <img src="{{url('/public/geo-logo1.png')}}" alt="GEO" width="200" height="auto">
     </a>
-    <a href="{{url('/')}}" class="nav-link text-end {{ request()->is('dash*') ? 'active' : '' }}">ڈیش بورڈ<i class="fas fa-chart-line icon"></i></a>
-    <a href="{{url('/employee')}}" class="nav-link {{ request()->is('employee*') ? 'active' : '' }}"><i class="fas fa-walking icon"></i>ملازم</a>
-    <a href="{{url('/accounts')}}"  class="nav-link {{ request()->is('account*') ? 'active' : '' }}"><i class="far fa-user-circle icon"></i>کھاتہ</a>
-    <a href="{{url('/invoices')}}" class="nav-link {{request()->is('invoice*') ? 'active' : ''}}"><i class="fas fa-cart-plus icon"></i>سیل</a>
-    <a href="{{url('/add_new_product')}}" class="nav-link {{request()->is('add_new_product*') ? 'active' : ''}}"><i class="fas fa-pen-alt icon"></i>نیا پروڈکٹ شامل کریں۔
-</a>
-    <a href="{{route('logout')}}" class="nav-link {{ request()->is('logout*') ? 'active' : '' }}"><i class="fas fa-sign-out-alt icon "></i>لاگ آوٹ
-</a>
+    <a href="{{url('/')}}" class="nav-link text-end {{ request()->is('dash*') ? 'active' : '' }}">ڈیش بورڈ  <i class="fas fa-chart-line icon fa-flip-horizontal"></i></a>
+    <a href="{{url('/employee')}}" class="nav-link text-end {{ request()->is('employee*') ? 'active' : '' }}">ملازم  <i class="fas fa-walking icon fa-flip-horizontal"></i></a>
+    <a href="{{url('/accounts')}}"  class="nav-link text-end {{ request()->is('account*') ? 'active' : '' }}">کھاتہ  <i class="far fa-user-circle icon"></i></a>
+    <a href="{{url('/invoices')}}" class="nav-link text-end {{request()->is('invoice*') ? 'active' : ''}}">سیل  <i class="fas fa-cart-plus icon fa-flip-horizontal"></i></a>
+    <a href="{{url('/add_new_product')}}" class="nav-link text-end {{request()->is('add_new_product*') ? 'active' : ''}}">نیا پروڈکٹ شامل کریں۔ <i class="fas fa-pen-alt icon"></i></a>
+    <a href="{{route('logout')}}" class="nav-link text-end {{ request()->is('logout*') ? 'active' : '' }}">لاگ آوٹ  <i class="fas fa-sign-out-alt icon fa-flip-horizontal"></i></a>
   </div>
 
 </nav>
